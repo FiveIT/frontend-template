6f28365d607a4583ec6b807042e069ceb7b55936
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-02 23:41:57 +0000

    Add linters and formatters

---
+++ client/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3',
+    },
+  ],
+  rules: {},
+}
