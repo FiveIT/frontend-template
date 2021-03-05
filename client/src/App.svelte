f45ebb8d2b02d6ee55118b594324041ae7233f1f
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-05 15:48:39 +0000

    Add fonts

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -3,4 +3,12 @@
   import { routes } from '../.routify/routes'
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Cardo&family=Carme&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
 <Router {routes} />
