2bf6788a87b2664b74bb4d756571987f157196bf
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-09 01:30:22 +0000

    Use Window component

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-  import { Router } from "@roxi/routify";
-  import { routes } from "../.routify/routes";
+  import { Router } from '@roxi/routify'
+  import { routes } from '../.routify/routes'
+  import Window from './components/Window.svelte'
 </script>
 
 <Router {routes} />
+
+<Window />
