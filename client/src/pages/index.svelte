b31829a596b9b85abab33ee740e26b4022c31b4c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-09 01:32:29 +0000

    Animate blobs, fix button disable bug, add keyboard event listeners

--- client/src/pages/index.svelte
+++ client/src/pages/index.svelte
@@ -2,49 +2,45 @@
   import { Carousel } from 'renderless-svelte'
   import type { CarouselControls } from 'renderless-svelte'
   import Queue from '../queue'
-  import { metatags } from '@roxi/routify'
+  import { metatags, ready } from '@roxi/routify'
+  import { store as orange } from '../components/blob/Orange.svelte'
+  import { store as red } from '../components/blob/Red.svelte'
+  import { store as blue } from '../components/blob/Blue.svelte'
+  import { store as window } from '../components/Window.svelte'
+  import { onMount } from 'svelte'
 
   metatags.title = 'Cat facts'
 
   const responses: Promise<string[]>[] = []
-  let response: string[]
-  let fetchStatus = ''
+  let response: string[] | undefined
 
   const getFact = async () => {
     const length = response?.length ?? 0
     if (length < 5) {
       if (responses.length < 2) {
-        fetchStatus = 'fetching'
         const res = fetch(
           'https://cat-fact.herokuapp.com/facts/random?amount=10'
         )
           .then(res => res.json())
-          .then((res: { text: string }[]) => {
-            fetchStatus = ''
-            return res.map(r => r.text)
-          })
-          .catch(err => {
-            fetchStatus = 'error'
-            throw err
-          })
+          .then((res: { text: string }[]) => res.map(r => r.text))
         responses.push(res)
       }
       if (length === 0) {
         response = await responses.shift()!
       }
     }
-    return response.shift()!
+    return response!.shift()!
   }
 
   let history = new Queue<Promise<string>>(10)
   let items: any[]
 
   const pushHistory = () => {
-    history.push(getFact())
+    const fact = getFact()
+    history.push(fact)
     items = history.array() as any[]
+    return fact
   }
-
-  pushHistory()
 
   type Handler = () => void
 
@@ -64,59 +60,92 @@
         break
     }
   }
+
+  let mounted = false
+
+  onMount(() => {
+    $orange = {
+      x: 0,
+      y: $window.height - orange.height,
+      scale: 1.2,
+      rotate: 0,
+      flip: {
+        x: 0,
+        y: 0,
+      },
+    }
+    $red = {
+      x: $window.width - red.width * 1.5,
+      y: $window.height - red.height * 0.45,
+      scale: 1.2,
+      rotate: 32.5,
+      flip: {
+        x: 1,
+        y: 0,
+      },
+    }
+    $blue = {
+      x: ($window.width - blue.width * 0.49) / 2,
+      y: -blue.height * 0.635 + $window.height * 0.1,
+      scale: 1.2,
+      rotate: 0,
+      flip: {
+        x: 0,
+        y: 1,
+      },
+    }
+    pushHistory().then($ready)
+    mounted = true
+  })
+
+  $: if (mounted) {
+    $orange.y = $window.height - orange.height
+    $red.x = $window.width - red.width * 1.5
+    $red.y = $window.height - red.height * 0.45
+    $blue.x = ($window.width - blue.width * 0.49) / 2
+    $blue.y = -blue.height * 0.635 + $window.height * 0.1
+  }
 </script>
 
-<main class="flex flex-col items-center p-10vh w-full min-h-screen bg-white">
-  <Carousel {items} let:payload bind:controls let:currentIndex let:setIndex>
-    <div class="flex flex-col items-center space-y-sm w-full min-h-full">
-      <h1
-        class="font-serif tracking-tighter text-xl subpixel-antialiasing font-bold select-none m-0"
-      >
-        Cat facts<span
-          class="text-orange transition"
-          class:text-blue={fetchStatus === 'fetching'}
-          class:text-red={fetchStatus === 'error'}>.</span
-        >
-      </h1>
-      {#await payload}
-        <p class="text text-blue">Retrieving a cat fact...</p>
-      {:then text}
-        <p class="text">{text}</p>
-      {:catch}
-        <p class="text text-red">Something terribly bad happened!</p>
-      {/await}
-    </div>
-    <div class="flex space-x-3 items-center mt-auto">
-      <button
-        class="control text"
-        on:click={controls.previous}
-        disabled={currentIndex === 0}>Previous</button
-      >
-      <button
-        class="control text"
-        on:click={controls.next}
-        disabled={currentIndex === history.length - 1}>Next</button
-      >
-    </div>
+<Carousel {items} let:payload bind:controls let:currentIndex let:setIndex>
+  <div class="flex flex-col items-center space-y-sm w-full min-h-full">
+    {#await payload}
+      <p class="text text-blue">Retrieving a cat fact...</p>
+    {:then text}
+      <p class="font-serif text-prose text-center max-w-prose">{text}</p>
+    {:catch}
+      <p class="text text-red">Something terribly bad happened!</p>
+    {/await}
+  </div>
+  <div class="flex space-x-3 items-center mt-auto">
     <button
-      class="mt-4 text"
-      on:click={() => (pushHistory(), setIndex(history.length - 1))}
-      >New fact</button
+      class="control text"
+      on:click={controls.previous}
+      disabled={currentIndex === 0}>Previous</button
     >
-  </Carousel>
-</main>
+    <button
+      class="control text"
+      on:click={controls.next}
+      disabled={!items || currentIndex === items.length - 1}>Next</button
+    >
+  </div>
+  <button
+    class="mt-4 text"
+    on:click={() => (pushHistory(), setIndex(history.length - 1))}
+    >New fact</button
+  >
+</Carousel>
+
+<svelte:window on:keydown={keydownHandler(controls.previous, controls.next)} />
 
 <style>
-  :global(html) {
-    font-size: 18px;
-  }
-
   .text {
-    @apply font-sans tracking-tighter text-base subpixel-antialiasing text-center max-w-prose;
+    @apply font-sans text-base subpixel-antialiasing text-center max-w-prose;
   }
 
   button {
-    @apply p-6 border rounded-xl transition bg-white cursor-pointer select-none outline-none;
+    backdrop-filter: blur(100px);
+    @apply p-6 border rounded-xl transition bg-white cursor-pointer select-none outline-none bg-opacity-50;
   }
 
   button:hover,
