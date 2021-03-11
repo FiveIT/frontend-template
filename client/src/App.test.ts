a611c59fd63339761fe9f82328916880c56a1fbc
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-11 17:14:58 +0000

    Remove test file

--- client/src/App.test.ts
+++
@@ -1,9 +0,0 @@
-// Broken by Routify
-//import '@testing-library/jest-dom/extend-expect'
-//import { render } from '@testing-library/svelte'
-//import App from './App.svelte'
-//
-//test('shows correct title', () => {
-//  const { getByText } = render(App)
-//  expect(getByText('Cat facts.')).toBeInTheDocument()
-//})
