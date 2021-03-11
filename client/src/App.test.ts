95237be25b935d509190e12c084a5494bf76546c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-11 17:12:39 +0000

    Write component test and comment it out

---
+++ client/src/App.test.ts
@@ -0,0 +1,9 @@
+// Broken by Routify
+//import '@testing-library/jest-dom/extend-expect'
+//import { render } from '@testing-library/svelte'
+//import App from './App.svelte'
+//
+//test('shows correct title', () => {
+//  const { getByText } = render(App)
+//  expect(getByText('Cat facts.')).toBeInTheDocument()
+//})
