93a6d7485409bc1226b505fdab66f96a6525a664
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-14 17:39:06 +0000

    Add redirect for Netlify

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/aws/aws-lambda-go v1.23.0
 	github.com/awslabs/aws-lambda-go-api-proxy v0.9.0
 	github.com/gofiber/fiber/v2 v2.5.0
+	github.com/gofiber/redirect/v2 v2.1.0
 	github.com/klauspost/compress v1.11.12 // indirect
 	github.com/rs/zerolog v1.20.0 // indirect
 	github.com/valyala/fasthttp v1.22.0 // indirect
--- internal/server/server.go
+++ internal/server/server.go
@@ -7,10 +7,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/redirect/v2"
 )
 
 func New() *fiber.App {
 	app := fiber.New()
+
+	// Redirect required for Netlify
+	app.Use(redirect.New(redirect.Config{
+		Rules: map[string]string{
+			"/.netlify/functions/index/*": "/$1",
+		},
+		StatusCode: http.StatusOK,
+	}))
 
 	app.Use(logger.New())
 
@@ -18,10 +27,12 @@ func New() *fiber.App {
 		AllowOrigins: meta.URL(),
 	}))
 
+	// Routes go here
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("sarmale cu ghimbir")
 	})
 
+	// 404 Not found handler
 	app.Use(func(c *fiber.Ctx) error {
 		return c.Status(http.StatusNotFound).SendString("nu am gasit acest loc")
 	})
