30ecacb836a1779473f7627031c4b478784ab97c
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-03-14 17:07:59 +0000

    Add logger

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,10 +4,13 @@ import (
 	"github.com/FiveIT/template/internal/meta"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func New() *fiber.App {
 	app := fiber.New()
+
+	app.Use(logger.New())
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: meta.URL(),
