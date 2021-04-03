152e5fa36f466323d877e8ebc5675a48bb2a8460
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-03 11:22:00 +0000

    Create components for fact generation and navigation

---
+++ web/src/pages/_components/FactNavButton.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Text from '$/components/text/Inline.svelte'
+  export let disabled = false
+</script>
+
+<button
+  {disabled}
+  on:click
+  class="blur p-sm border rounded transition bg-white bg-opacity-50 cursor-pointer select-none outline-none hover:(bg-blue border-blue text-white) focus-visible:(bg-blue border-blue text-white) disabled:(pointer-events-none opacity-50)">
+  <Text>
+    <slot />
+  </Text>
+</button>
