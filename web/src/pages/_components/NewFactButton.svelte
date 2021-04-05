58cd851f93c4e0ffd04ab9ef5cf03d09080b0288
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-05 14:12:01 +0000

    Remove focus outline

--- web/src/pages/_components/NewFactButton.svelte
+++ web/src/pages/_components/NewFactButton.svelte
@@ -7,7 +7,7 @@
 <button
   on:click
   {disabled}
-  class="blur p-lg border rounded transition bg-white bg-opacity-50 cursor-pointer select-none outline-none hover:(border-orange bg-orange text-white) focus-visible:(border-orange bg-orange text-white)">
+  class="blur p-lg border rounded transition bg-white bg-opacity-50 cursor-pointer select-none hover:(border-orange bg-orange text-white) focus-visible:(border-orange bg-orange text-white) focus:outline-transparent">
   <Text>
     <slot />
   </Text>
