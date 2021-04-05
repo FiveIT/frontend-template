7b60d4fe80ba5533d145603b8584155556d85857
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2021-04-05 17:11:43 +0000

    Write more tests

--- web/cypress/integration/home.spec.js
+++ web/cypress/integration/home.spec.js
@@ -1,11 +1,7 @@
 /// <reference types="cypress" />
 
 describe('Home', () => {
-  beforeEach(() => {
-    cy.visit('/')
-  })
-
   it('should have "Cat Facts" as title', () => {
-    cy.get('h1').contains('Cat facts')
+    cy.visit('/').get('h1').contains('Cat facts')
   })
 })
